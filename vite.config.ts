d5e84ad47dc94c821a653fc9b2e360a2b4124ffc
Abhiseck Bhattacharya <abhiseck@outlook.com>
2025-04-01 10:55:15 +0530

    chore: update API URL in .env and enable auto code splitting in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
